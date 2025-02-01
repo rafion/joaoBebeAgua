9ca5c14eb99dd5247b6898bbd07a295510481687
rafion <rafion.torres@gmail.com>
2025-02-01 10:51:33 -0300

    href null to hide tab bar orders

--- src/app/(drawer)/(tabs)/_layout.tsx
+++ src/app/(drawer)/(tabs)/_layout.tsx
@@ -24,6 +24,7 @@ export default function TabLayout() {
             <Tabs.Screen
                 name="orders"
                 options={{
+                    href: null,
                     title: 'Pedidos',
                     headerTitle: 'Pedido',
                     tabBarIcon: ({ size, color }) => (<MaterialIcons name="app-registration" size={size} color={color} />)
